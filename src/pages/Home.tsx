d2230e0a006f9f19d122f11b9d297014fb4e2142
ISABROTHER <ishimpatrickjohnston@gmail.com>
2025-11-25 16:12:13 +0100

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -227,7 +227,7 @@ export function Home({ onNavigate }: HomeProps) {
         </div>
       </section>
 
-      {/* === 4. PRIORITIES PREVIEW === */}
+      {/* === 4. PRIORITIES PREVIEW === */} 
       <section className="py-20 bg-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center max-w-3xl mx-auto mb-16">
